65566cf04e7aa65b22c2ac1d08fcd9269c8678dd
Wilbert Ribeiro <wkelyson@gmail.com>
2017-03-18 21:48:56 -0300

    Add api_server URL.

--- config/environment.js
+++ config/environment.js
@@ -29,6 +29,7 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
+    ENV.api_server = 'localhost:3000'
   }
 
   if (environment === 'test') {
