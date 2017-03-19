180ad0f77c956ca4107871d61086e5c2d0bfda5b
Wilbert Ribeiro <wkelyson@gmail.com>
2017-03-19 11:17:58 -0300

    Enable API URL from environment.js

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -3,6 +3,6 @@ import config from '../config/environment';
 
 export default DS.RESTAdapter.extend({
   // namespace: 'api/v1',
-  // host: config.api_server
-  host: 'http://127.0.0.1:3000'
+  host: config.api_server
+  // host: 'http://127.0.0.1:3000'
 });
--- config/environment.js
+++ config/environment.js
@@ -29,7 +29,7 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
-    ENV.api_server = 'localhost:3000'
+    ENV.api_server = 'http://127.0.0.1:3000'
   }
 
   if (environment === 'test') {
@@ -41,10 +41,11 @@ module.exports = function(environment) {
     ENV.APP.LOG_VIEW_LOOKUPS = false;
 
     ENV.APP.rootElement = '#ember-testing';
+    ENV.api_server = 'http://127.0.0.1:3000'
   }
 
   if (environment === 'production') {
-
+    ENV.api_server = 'http://127.0.0.1:3000'
   }
 
   return ENV;
