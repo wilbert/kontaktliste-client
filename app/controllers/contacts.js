6e1785327d9bd5853b161b23b9cea2a722a03cd1
Wilbert Ribeiro <wkelyson@gmail.com>
2017-03-19 13:46:31 -0300

    Add name as default sort param.

--- app/controllers/contacts.js
+++ app/controllers/contacts.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   queryParams: ['term', 'sort'],
   filterTerm: '',
-  sort: '',
+  sort: 'name',
   actions: {
     searchContacts(term, sort) {
       this.transitionToRoute('contacts', { queryParams: { term: term, sort: sort } });
