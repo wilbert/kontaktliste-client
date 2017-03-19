d17d38be5b71c4a0dca877cfa9f883c2156239c7
Wilbert Ribeiro <wkelyson@gmail.com>
2017-03-18 21:15:23 -0300

    Add model contact fetch in contacts route.

--- app/routes/contacts.js
+++ app/routes/contacts.js
@@ -3,5 +3,8 @@ import config from '../config/environment';
 
 export default Ember.Route.extend({
   location: config.locationType,
-  rootURL: config.rootURL
+  rootURL: config.rootURL,
+  model(params) {
+    return this.store.query('contact', params);
+  }
 });
